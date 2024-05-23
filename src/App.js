1203b04d1d2e2f482161d2ec10b603df7fab16b7
rakesh1645 <rakeshchoudhary902422@gmail.com>
2024-05-23 17:03:24 +0530

    The Project is almost complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './Component/Navbar';
+import Text from './Component/Text';
+import Alert from './Component/Alert';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from './Component/About';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleBtn = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = 'black';
+      showAlert("The dark mode is off", "success");
+      document.title = "Text-changer light mode";
+    } else {
+      setMode('dark');
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = 'white';
+      showAlert("The dark mode is on", "success");
+      document.title = "Text-changer dark mode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         rakesh
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar title="Rakesh" mode={mode} toggleBtn={toggleBtn} showAlert={showAlert} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route exact path="/" element={<Text heading="Enter text here" mode={mode} showAlert={showAlert} />} />
+        <Route exact path="/about" element={<About showAlert={showAlert} alert={alert} />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
   );
 }
 
